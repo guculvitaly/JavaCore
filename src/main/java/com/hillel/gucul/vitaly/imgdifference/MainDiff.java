728f8199ab38b7637c698ba0c6053340e452ea69
Gucul Vitaly <riseagainst232323@gmail.com>
2017-10-06 11:42:22 +0300

    add comments and write defoult path to imgs

--- src/main/java/com/hillel/gucul/vitaly/imgdifference/MainDiff.java
+++ src/main/java/com/hillel/gucul/vitaly/imgdifference/MainDiff.java
@@ -11,14 +11,20 @@ public final class MainDiff {
     private MainDiff (){}
 
 
+    /**
+     *
+     * @param img1
+     * @param img2
+     * @return
+     */
     public static BufferedImage getDiffImg(BufferedImage img1, BufferedImage img2){
-
+        //make images to arrays of pixel
         final int width = img1.getWidth();
         final int height = img1.getHeight();
         final int result = Color.RED.getRGB();
         final int [] pixFirst = img1.getRGB(0,0,width,height, null, 0, width);
         final int [] pixSecond = img2.getRGB(0,0,width,height, null, 0, width);
-
+        //comp images pixel by pixel
         for (int i = 0; i < pixFirst.length ; i++) {
             if (pixFirst[i] != pixSecond[i]){
                 pixFirst[i] = result;
@@ -30,14 +36,20 @@ public final class MainDiff {
         return outImg;
     }
 
+    /**
+     *
+     * @param args
+     * @throws IOException
+     */
+
     public static void main(String[] args) throws IOException {
 
         ImageIO.write(
                 getDiffImg(
-                        ImageIO.read(new File("C:\\Users\\insomnia\\IdeaProjects\\Testing\\gucul_home_work\\src\\main\\resources\\img\\img1.png")),
-                        ImageIO.read(new File("C:\\Users\\insomnia\\IdeaProjects\\Testing\\gucul_home_work\\src\\main\\resources\\img\\img2.png"))),
+                        ImageIO.read(new File("your_path_to_img1")),
+                        ImageIO.read(new File("your_path_to_img2"))),
                 "png",
-                new File("C:\\Users\\insomnia\\IdeaProjects\\Testing\\gucul_home_work\\src\\main\\resources\\outputImg.png"));
+                new File("your_out_path_to_img"));
     }
 
 
